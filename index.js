d807da80abeb05457777654c8e9648de9dc350d5
nirmalyachk <137274795+nirmalyachk@users.noreply.github.com>
2023-06-22 06:15:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (req, res) {
+
+res.send("Simple Web Application is UP and hi from "+ req.headers.host);
+
+});
+
+app.listen(8081, function () {
+
+console.log("Simple Web Application running on port 8081!");
+
+});
